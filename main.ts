87143095e9a361b394c3c5a19de5922f0597c821
junhakjang <100299253+junhakjang@users.noreply.github.com>
2022-09-07 14:09:44 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hi")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Bye")
+})
+basic.showIcon(IconNames.Skull)
+basic.forever(function () {
+	
+})
